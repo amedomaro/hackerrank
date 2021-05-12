972abc53abd3c674d0c52104ab32ac0b0d8d8c98
Amedomaro <amedomaro13@gmail.com>
2021-05-12 16:11:13 +0300

    add Task

---
+++ src/com/company/easy/stdin_and_stdout/Solution.java
@@ -0,0 +1,17 @@
+package com.company.easy.stdin_and_stdout;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
