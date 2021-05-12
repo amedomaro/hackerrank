d0ae26c6065b210b6d911a046db6e4bc5e8bd05e
Amedomaro <amedomaro13@gmail.com>
2021-05-12 20:09:16 +0300

    Solution

--- src/com/company/easy/java_loops_I/Solution.java
+++ src/com/company/easy/java_loops_I/Solution.java
@@ -8,8 +8,14 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
+        int n = scanner.nextInt();
+        int result;
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for(int i = 1; i <= 10; i++){
+            result = n*i;
+            System.out.printf("%d x %d = %d\n", n, i, result);
+        }
 
         scanner.close();
     }
