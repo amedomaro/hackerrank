b9a54b01763400f589e12108a6c576eac831a50c
Amedomaro <amedomaro13@gmail.com>
2021-05-12 16:25:55 +0300

    add Task

--- src/com/company/easy/stdin_and_stdout/Solution.java
+++ src/com/company/easy/stdin_and_stdout/Solution.java
@@ -6,12 +6,15 @@ public class Solution {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
 
-        // Write your code here.
-
-        System.out.println("String: " + s);
+        System.out.printf("String: %s\n", s);
         System.out.println("Double: " + d);
-        System.out.println("Int: " + i);
+        System.out.printf("Int: %d\n", i);
     }
 }
