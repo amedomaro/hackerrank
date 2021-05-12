018e8e6806db0f4ab6b6a59e4e3e0b3f73a6eeb9
Amedomaro <amedomaro13@gmail.com>
2021-05-12 21:00:01 +0300

    Solution

--- src/com/company/easy/java_loops_II/Solution.java
+++ src/com/company/easy/java_loops_II/Solution.java
@@ -2,14 +2,21 @@ package com.company.easy.java_loops_II;
 
 import java.util.*;
 
-class Solution{
-    public static void main(String []argh){
+class Solution {
+    public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
+
+            // Solution
+            for (int j = 0; j < n; j++) {
+                a += Math.pow(2, j) * b;
+                System.out.printf("%d ", a);
+            }
+            System.out.println();
         }
         in.close();
     }
