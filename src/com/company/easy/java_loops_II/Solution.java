2ea8496c57a8ee9c7a5caf3816dc4175308fd2d3
Amedomaro <amedomaro13@gmail.com>
2021-05-12 20:36:31 +0300

    add Task

---
+++ src/com/company/easy/java_loops_II/Solution.java
@@ -0,0 +1,16 @@
+package com.company.easy.java_loops_II;
+
+import java.util.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+        }
+        in.close();
+    }
+}
