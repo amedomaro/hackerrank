fb03a8341613e40048ca1bddd61c976dff0a7937
Amedomaro <amedomaro13@gmail.com>
2021-05-12 16:00:59 +0300

    Solution

--- src/com/company/easy/ifElse/Solution.java
+++ src/com/company/easy/ifElse/Solution.java
@@ -7,8 +7,12 @@ public class Solution {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
+        int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        if ((n % 2 == 0) && (n > 2 && n < 5) || (n % 2 == 0) && (n > 20)) {  // Solution
+            System.out.println("Not Weird");
+        } else System.out.print("Weird");
 
         scanner.close();
     }
