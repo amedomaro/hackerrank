75775be2d25be14d17adc42f578da2ebd7d5dff8
Amedomaro <amedomaro13@gmail.com>
2021-05-13 17:19:57 +0300

    Solution

--- src/com/company/easy/static_initializer_block/Solution.java
+++ src/com/company/easy/static_initializer_block/Solution.java
@@ -1,21 +1,30 @@
 package com.company.easy.static_initializer_block;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
     //Write your code here
+    static int b, h;
+    static boolean flag;
+    static Scanner scan = new Scanner(System.in);
 
-    public static void main(String[] args){
-        if(flag){
-            int area=B*H;
+    static {
+        b = scan.nextInt();
+        h = scan.nextInt();
+        flag = b > 0 && h > 0;
+        if (!flag) System.out.println("java.lang.Exception: Breadth and height must be positive");
+    }
+
+
+    public static void main(String[] args) {
+        if (flag) {
+            int area = b * h;
             System.out.print(area);
         }
-
+//        else {
+//            System.out.println("java.lang.Exception: Breadth and height must be positive");
+//        }
     }//end of main
 
 }//end of class
