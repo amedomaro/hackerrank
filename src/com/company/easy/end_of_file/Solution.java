87ccd637a3777c400532ec34d24019106ef1aaf9
Amedomaro <amedomaro13@gmail.com>
2021-05-13 10:45:11 +0300

    add Task

---
+++ src/com/company/easy/end_of_file/Solution.java
@@ -0,0 +1,8 @@
+package com.company.easy.end_of_file;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
