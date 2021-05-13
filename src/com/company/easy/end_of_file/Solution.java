84a9b8fcc8efe21f3cf87bc58715219aa965ff6c
Amedomaro <amedomaro13@gmail.com>
2021-05-13 10:57:33 +0300

    Solution

--- src/com/company/easy/end_of_file/Solution.java
+++ src/com/company/easy/end_of_file/Solution.java
@@ -1,8 +1,18 @@
 package com.company.easy.end_of_file;
 
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scan = new Scanner(System.in);
+        String str;
+        int count = 1;
+
+        while (scan.hasNextLine()){
+            str = scan.nextLine();
+            System.out.printf("%d %s\n", count, str);
+            count++;
+        }
     }
 }
