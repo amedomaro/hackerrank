19f8cf0ab078b3d76490a089682b196c561af884
Amedomaro <amedomaro13@gmail.com>
2021-05-13 10:11:29 +0300

    Solution

--- src/com/company/easy/datatypes/Solution.java
+++ src/com/company/easy/datatypes/Solution.java
@@ -4,25 +4,25 @@ import java.util.*;
 import java.io.*;
 
 
-class Solution{
-    public static void main(String []argh) {
+class Solution {
+    public static void main(String[] argh) {
 
         Scanner sc = new Scanner(System.in);
-        int t=sc.nextInt();
+        int t = sc.nextInt();
 
-        for(int i=0;i<t;i++)
-        {
+        for (int i = 0; i < t; i++) {
 
-            try
-            {
-                long x=sc.nextLong();
-                System.out.println(x+" can be fitted in:");
-                if(x>=-128 && x<=127)System.out.println("* byte");
-                //Complete the code
-            }
-            catch(Exception e)
-            {
-                System.out.println(sc.next()+" can't be fitted anywhere.");
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                // Solution
+                if (x >= Byte.MIN_VALUE && x <= Byte.MAX_VALUE) System.out.println("* byte");
+                if (x >= Short.MIN_VALUE && x <= Short.MAX_VALUE) System.out.println("* short");
+                if (x >= Integer.MIN_VALUE && x <= Integer.MAX_VALUE) System.out.println("* int");
+                System.out.println("* long");
+
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
             }
         }
     }
