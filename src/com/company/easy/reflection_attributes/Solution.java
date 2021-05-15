63acab40e0074b57618c7af0b1eed4905be2b508
Amedomaro <amedomaro13@gmail.com>
2021-05-15 23:50:26 +0300

    add Task

---
+++ src/com/company/easy/reflection_attributes/Solution.java
@@ -0,0 +1,19 @@
+package com.company.easy.reflection_attributes;
+
+public class Solution {
+
+    public static void main(String[] args){
+        Class student = ~~~Complete this line~~~;
+        Method[] methods = ~~~Complete this line~~~;
+
+        ArrayList<String> methodList = new ArrayList<>();
+        for(~~~Complete this line~~~){
+            methodList.add(~~~Complete this line~~~);
+        }
+        Collections.sort(methodList);
+        for(String name: methodList){
+            System.out.println(name);
+        }
+    }
+
+}
