76b14881b32151d953435f0041db5181b7695add
Amedomaro <amedomaro13@gmail.com>
2021-05-15 23:32:50 +0300

    Solution

--- src/com/company/easy/currency_formatter/Solution.java
+++ src/com/company/easy/currency_formatter/Solution.java
@@ -1,11 +1,25 @@
 package com.company.easy.currency_formatter;
 
 import java.io.*;
+import java.text.NumberFormat;
 import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        double payment = sc.nextDouble();
+        sc.close();
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(new Locale("en", "IN")).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+
+        System.out.printf("US: %s\n", us);
+        System.out.printf("India: %s\n", india);
+        System.out.printf("China: %s\n", china);
+        System.out.printf("France: %s\n", france);
     }
 }
