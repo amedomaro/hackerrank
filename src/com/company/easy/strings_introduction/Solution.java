5f0585293284a7f918baa45d64ed81e99f3ff39d
Amedomaro <amedomaro13@gmail.com>
2021-05-16 23:35:41 +0300

    Solution

--- src/com/company/easy/strings_introduction/Solution.java
+++ src/com/company/easy/strings_introduction/Solution.java
@@ -1,16 +1,24 @@
 package com.company.easy.strings_introduction;
 
-import java.io.*;
 import java.util.*;
 
 public class Solution {
 
+    private static String upFirstLetter(String str) {
+        if (str == null || str.length() == 0) return "";
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
+    }
+
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        String B=sc.next();
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
         /* Enter your code here. Print output to STDOUT. */
+
+        System.out.println(a.length() + b.length());
+        System.out.println(a.compareTo(b) > 0 ? "Yes" : "No");
+        System.out.println(upFirstLetter(a) + " " + upFirstLetter(b));
 
     }
 }
