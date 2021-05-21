b62d0583f84d75d92e5ec450d90ba1ca694db8f2
Amedomaro <amedomaro13@gmail.com>
2021-05-21 20:51:32 +0300

    Solution

--- src/com/company/easy/string_reverse/Solution.java
+++ src/com/company/easy/string_reverse/Solution.java
@@ -7,9 +7,10 @@ public class Solution {
 
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
         /* Enter your code here. Print output to STDOUT. */
-
+        StringBuilder sb = new StringBuilder(a);
+        System.out.println(a.equals(sb.reverse().toString()) ? "Yes" : "No");
     }
 }
