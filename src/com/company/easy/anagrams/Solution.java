66e866b1ae1d9e2a253d21e8139c51e317932864
Amedomaro <amedomaro13@gmail.com>
2021-05-21 21:53:35 +0300

    Wrong Answer - Test case 6 and Test case 7

--- src/com/company/easy/anagrams/Solution.java
+++ src/com/company/easy/anagrams/Solution.java
@@ -1,11 +1,30 @@
 package com.company.easy.anagrams;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
 
     static boolean isAnagram(String a, String b) {
         // Complete the function
+        a = a.toLowerCase();
+        StringBuilder sb = new StringBuilder(b.toLowerCase());
+        boolean flag = false;
+
+        if (a.length() == b.length()) {
+
+            for (int i = 0; i < a.length(); i++) {
+                for (int j = 0; j < sb.length(); j++) {
+
+                    if (a.charAt(i) == sb.charAt(j)) {
+                        sb.deleteCharAt(j);
+                    }
+                    if (sb.length() == 0) {
+                        flag = (i == a.length() - 1);
+                    }
+                }
+            }
+        }
+        return flag;
     }
 
     public static void main(String[] args) {
@@ -15,6 +34,6 @@ public class Solution {
         String b = scan.next();
         scan.close();
         boolean ret = isAnagram(a, b);
-        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+        System.out.println((ret) ? "Anagrams" : "Not Anagrams");
     }
 }
