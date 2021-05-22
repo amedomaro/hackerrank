b4f64543bd9e9fae6351303d6635da428bcbc1df
Amedomaro <amedomaro13@gmail.com>
2021-05-22 21:48:15 +0300

    Add Task

---
+++ src/com/company/easy/string_tokens/Solution.java
@@ -0,0 +1,14 @@
+package com.company.easy.string_tokens;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+    }
+}
