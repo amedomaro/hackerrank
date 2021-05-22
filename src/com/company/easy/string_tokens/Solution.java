6c8fd11889ea6b7823e10be54438d743d01a5da6
Amedomaro <amedomaro13@gmail.com>
2021-05-22 23:07:21 +0300

    Solution

--- src/com/company/easy/string_tokens/Solution.java
+++ src/com/company/easy/string_tokens/Solution.java
@@ -9,6 +9,27 @@ public class Solution {
         Scanner scan = new Scanner(System.in);
         String s = scan.nextLine();
         // Write your code here.
+
+        StringBuilder str = new StringBuilder();
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (Character.isLetter(s.charAt(i))) {
+                str.append(s.charAt(i));
+            } else if (str.length() != 0) {
+                list.add(str.toString());
+                str.delete(0, str.length());
+            }
+        }
+
+        if (str.length() > 0 && !str.toString().equals(list.get(list.size()-1))){ // Check str
+            list.add(str.toString());
+        }
+
+        System.out.println(list.size());
+        list.forEach(System.out::println);
+
         scan.close();
     }
 }
