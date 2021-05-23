2089e24a7e85ad237a9f2327f08acb9e81cbeef4
Amedomaro <amedomaro13@gmail.com>
2021-05-23 14:45:42 +0300

    Solution

--- src/com/company/easy/map/Solution.java
+++ src/com/company/easy/map/Solution.java
@@ -1,24 +1,34 @@
 package com.company.easy.map;
 
 //Complete this code or write your own from scratch
+
 import java.util.*;
 import java.io.*;
 
-class Solution{
-    public static void main(String []argh)
-    {
+class Solution {
+
+    public static void main(String[] argh) {
         Scanner in = new Scanner(System.in);
-        int n=in.nextInt();
-        in.nextLine();
-        for(int i=0;i<n;i++)
-        {
-            String name=in.nextLine();
-            int phone=in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<>();
+        String name;
+        int phone, n = in.nextInt();
+        in.nextLine();    // read line break
+
+        for (int i = 0; i < n; i++) {
+            name = in.nextLine();
+            phone = in.nextInt();
+            phoneBook.put(name, phone);
             in.nextLine();
         }
-        while(in.hasNext())
-        {
-            String s=in.nextLine();
+        while (in.hasNext()) {
+            String s = in.nextLine();
+            if (phoneBook.containsKey(s)) {
+                System.out.printf("%s=%d\n", s, phoneBook.get(s));
+            } else {
+                System.out.println("Not found");
+            }
         }
+
+        in.close(); //close scanner
     }
 }
