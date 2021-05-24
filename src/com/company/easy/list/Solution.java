ca35649dbf9320caf1be157fe409e6db17924b9c
Amedomaro <amedomaro13@gmail.com>
2021-05-24 23:22:37 +0300

    Add Task

---
+++ src/com/company/easy/list/Solution.java
@@ -0,0 +1,13 @@
+package com.company.easy.list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+    }
+}
